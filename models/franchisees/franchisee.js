ed2c48427da271551fdd24edaacb31cd0494a0e6
ikshit1 <ikshit1@gmail.com>
2018-02-09 10:23:16 +0530

    changes in the franchisee model

--- models/franchisees/franchisee.js
+++ models/franchisees/franchisee.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
+require('../authenticate/authenticate.js');
 var Schema = mongoose.Schema;
 var franchiseeSchema = new mongoose.Schema({
+    "franchisee_code":{ type: Schema.Types.ObjectId, ref: 'Auth'},
     "franchisee_name":String,
     "franchisee_occupation":String,
     "franchisee_email":String,
@@ -21,8 +23,17 @@ var franchiseeSchema = new mongoose.Schema({
 });
 
 var librarySchema = new mongoose.Schema({
-    franchisee_pic:{type: Schema.Types.Mixed, default : {}}
+    "common_files":{type: Schema.Types.Mixed, default : {}},
+    "uploaded_status":{type:'Number',default:0}//0 or 1
+});
+
+var UserlibrarySchema = new mongoose.Schema({
+    "personal_files":{type: Schema.Types.Mixed, default : {}},
+    "uploaded_status":{type:'Number',default:0},//0 or 1
+    "franchisee_Id":{ type: Schema.Types.ObjectId, ref: 'Franchisee'}
 });
 
 mongoose.model('Franchisee', franchiseeSchema);
 mongoose.model('Library', librarySchema);
+mongoose.model('UserLibrary', UserlibrarySchema);
+mongoose.model('Auth').Schema;
