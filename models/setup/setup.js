193684f40ee61ddc2ce4da5d2d1d35ca99debc76
swamywta <swamy@wtastudios.com>
2018-05-17 20:48:16 +0530

    changes in setup API

--- models/setup/setup.js
+++ models/setup/setup.js
@@ -34,10 +34,30 @@ var setupChecklistSchema = new mongoose.Schema({
   setup_checklist_name: String,
   setup_checklist_name_EN: String,
   created_at: String
- 
+
+})
+
+var userBasedSetupTaskSchema = new mongoose.Schema({
+  setup_checklist_id: {type: Schema.Types.ObjectId, ref: 'SetupTask'},
+  task_name_EN: String,
+  task_name: String,
+  task_status: {type:Boolean,default:false},
+  task_type: String,
+  franchisee_file_upload_required: {type:Boolean,default:false},
+  franchisor_task_file_attachment_file_name: String,
+  franchisor_task_file_attachment_file_url: String,
+  franchisor_task_file_attachment_file_type: String,
+  task_serial_number: Number,
+  task_radio_options: Array,
+  doc_name: String,
+  link: {type: Schema.Types.Mixed, default : {}},  "file_type": String,
+  date_uploaded:Date,
+  key:String,
+  franchisee_id:  {type: Schema.Types.ObjectId, ref: 'Franchisee'}
 })
 
 mongoose.model('SetupTask', setupTaskSchema);
 mongoose.model('SetupDepartment', setupDepartmentSchema);
 mongoose.model('SetupChecklist', setupChecklistSchema);
+mongoose.model('UserBasedSetupTask', userBasedSetupTaskSchema);
 //mongoose.model('')
