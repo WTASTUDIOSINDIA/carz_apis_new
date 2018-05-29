79f9164da631c0c16cf08c37711ec8bea76df6b2
apple <apple@apples-MacBook-Pro.local>
2018-05-29 09:38:09 +0530

    changes in API

--- routes/franchisees/franchisee.js
+++ routes/franchisees/franchisee.js
@@ -258,21 +258,21 @@ router.post('/create_franchisee',upload.single('franchisee_img'),function(req, r
             if(!franchisee){
                var franchisee = new Franchisee();
               //  franchisee.franchisee_code = franchiseeForm.franchisee_code,
-                franchisee.franchisee_name=franchiseeForm.franchisee_name,
-                if(!franchisee.franchisee_name){
+                franchisee.franchisee_name=franchiseeForm.franchisee_name;
+                if(!franchiseeForm.franchisee_name){
                   franchisee.franchisee_name=franchiseeForm.partner_name;
                 };
-                franchisee.franchisee_email=franchiseeForm.franchisee_email,
-                franchisee.franchisee_occupation=franchiseeForm.partner_occupation,
-                franchisee.franchisee_city=franchiseeForm.franchisee_city,
-                franchisee.franchisee_state=franchiseeForm.franchisee_state,
-                franchisee.franchisee_address=franchiseeForm.franchisee_address,
-                franchisee.franchisee_mobile_number=franchiseeForm.partner_mobile_number,
-                franchisee.franchisee_investment=franchiseeForm.franchisee_investment,
-                franchisee.franchisee_preferred_date=franchiseeForm.franchisee_preferred_date,
-                franchisee.franchisee_preferred_time=franchiseeForm.franchisee_preferred_time,
-                franchisee.franchisee_how_soon_to_start=franchiseeForm.franchisee_how_soon_to_start,
-                franchisee.franchisee_franchise_model=franchiseeForm.franchisee_franchise_model,
+                franchisee.franchisee_email=franchiseeForm.franchisee_email;
+                franchisee.franchisee_occupation=franchiseeForm.partner_occupation;
+                franchisee.franchisee_city=franchiseeForm.franchisee_city;
+                franchisee.franchisee_state=franchiseeForm.franchisee_state;
+                franchisee.franchisee_address=franchiseeForm.franchisee_address;
+                franchisee.franchisee_mobile_number=franchiseeForm.partner_mobile_number;
+                franchisee.franchisee_investment=franchiseeForm.franchisee_investment;
+                franchisee.franchisee_preferred_date=franchiseeForm.franchisee_preferred_date;
+                franchisee.franchisee_preferred_time=franchiseeForm.franchisee_preferred_time;
+                franchisee.franchisee_how_soon_to_start=franchiseeForm.franchisee_how_soon_to_start;
+                franchisee.franchisee_franchise_model=franchiseeForm.franchisee_franchise_model;
                 franchisee.franchisee_franchise_type=franchiseeForm.franchisee_franchise_type,
                 franchisee.franchisee_remarks=franchiseeForm.franchisee_remarks,
                 franchisee.franchisee_country=franchiseeForm.franchisee_country,
