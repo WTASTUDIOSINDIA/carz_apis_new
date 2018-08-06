d0c57457c13e5a5b601acdaf566dafb64f695307
hareeshvudari <vudari1043@gmail.com>
2018-08-06 10:37:52 +0530

    dddd

--- routes/assessment/assessment.js
+++ routes/assessment/assessment.js
@@ -177,9 +177,10 @@ router.post('/question_list',function(req,res){
 	}
 });
 
-router.get('/get_question_list/:question_section_id',function(req,res){
+// router.get('/get_question_list/:question_section_id',function(req,res){
+    router.get('/get_question_list',function(req,res){
     try{
-        Question.find({question_section_id:req.params.question_section_id},function(err,ques){
+        Question.find({},function(err,ques){
             if(err){
                 return res.send({
                     state:"error",
