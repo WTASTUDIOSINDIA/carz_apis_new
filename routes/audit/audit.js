ad3cd54c23cf14fe98ab42a59c27dcc48d09c2bd
saivishnuedala <saivishnu.edala@gmail.com>
2018-11-14 14:12:13 +0530

    changes and issues

--- routes/audit/audit.js
+++ routes/audit/audit.js
@@ -415,7 +415,7 @@ router.put('/update_audit_checklist', function(req, res) {
 });
 
 // to get checklist by id
-router.get('/get_audit_checklist_by_id/:checklist_type_id', utils.authenticated, function (req, res) {
+router.get('/get_audit_checklist_by_id/:checklist_type_id', function (req, res) {
     try {
       AuditChecklist.find({ checklist_type_id: req.params.checklist_type_id }, function (err, audit_checklist) {
         if (err) {
@@ -658,7 +658,7 @@ router.put('/update_audit_checklist_tasks', upload.single('audit_checklist_task_
   })
 
 // to get checklist tasks by checklist id
-router.get('/get_audit_checklist_tasks/:checklist_id', utils.authenticated, function (req, res) {
+router.get('/get_audit_checklist_tasks/:checklist_id', function (req, res) {
     try {
       AuditTask.find({ checklist_id: req.params.checklist_id }, function (err, task) {
         if (err) {
