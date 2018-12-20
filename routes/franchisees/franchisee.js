7a8b438803e11331fabb59c9b9c68fb437272058
saivishnuedala <saivishnu.edala@gmail.com>
2018-12-20 14:13:13 +0530

    fb

--- routes/franchisees/franchisee.js
+++ routes/franchisees/franchisee.js
@@ -62,6 +62,135 @@ var upload = multer({
     })
 });
 
+
+
+
+// 'use strict';
+// const bizSdk = require('facebook-nodejs-business-sdk');
+// const Lead = bizSdk.Lead;
+
+// const access_token = '1994150194009398|-7JyOIiFJj8pGPAmvCzKbZv8WB4';
+// const app_secret = 'c960c8fc254604e66df3001702456483';
+// const app_id = '1994150194009398';
+// const id = '<ID>';
+// const api = bizSdk.FacebookAdsApi.init(access_token);
+// const showDebugingInfo = true; // Setting this to true shows more debugging info.
+// if (showDebugingInfo) {
+//   api.setDebug(true);
+// }
+
+// const logApiCallResult = (apiCallName, data) => {
+//   console.log(apiCallName);
+//   if (showDebugingInfo) {
+//     console.log('Data:' + JSON.stringify(data));
+//   }
+// };
+
+// let fields, params;
+// fields = [
+// ];
+// params = {
+// };
+// const sample_code = (new Lead(id)).get(
+//   fields,
+//   params
+// );
+// logApiCallResult('sample_code api call complete.', sample_code);
+
+// Endpoint for verifying webhook subscripton
+// router.get('/leadgen', (req, res) => {
+//   if (!req.query) {
+//     res.send({success: false, reason: 'Empty request params'});
+//     return;
+//   }
+//   // Extract a verify token we set in the webhook subscription and a challenge to echo back.
+//   const verifyToken = req.query['hub.verify_token'];
+//   const challenge = req.query['hub.challenge'];
+
+//   if (!verifyToken || !challenge) {
+//     res.send({
+//       success: false,
+//       reason: 'Missing hub.verify_token and hub.challenge params',
+//     });
+//     return;
+//   }
+
+//   if (verifyToken !== VERIFY_TOKEN) {
+//     res.send({success: false, reason: 'Verify token does not match'});
+//     return;
+//   }
+//   // We echo the received challenge back to Facebook to finish the verification process.
+//   res.send(challenge);
+// });
+
+
+const GRAPH_API_VERSION = 'v3.2';
+const GRAPH_API_ENDPOINT = `https://graph.facebook.com/${GRAPH_API_VERSION}`;
+// Your system user access token file path.
+// Note: Your system user needs to be an admin of the subscribed page.
+const ACCESS_TOKEN_PATH = path.join(
+  __dirname,
+  '..',
+  '..',
+  'token',
+  'access_token.txt'
+);
+// var ACCESS_TOKEN_PATH = require('../../token/access_token')
+
+// Read access token for calling graph API.
+if (!fs.existsSync(ACCESS_TOKEN_PATH)) {
+  console.log(`The file ${ACCESS_TOKEN_PATH} does not exist.`);
+  process.exit(1);
+}
+const accessToken = fs.readFileSync(ACCESS_TOKEN_PATH, 'utf-8');
+// Facebook will post realtime leads to this endpoint if we've already subscribed to the webhook in part 1.
+router.get('/leadgen', (req, res) => {
+  const entry = req.query;
+console.log(req.query,'+++++++++++++++++++++++')
+  entry.forEach(page => {
+    page.changes.forEach(change => {
+      // We get page, form, and lead IDs from the change here.
+      // We need the lead gen ID to get the lead data.
+      // The form ID and page ID are optional. You may want to record them into your CRM system.
+      const {page_id, form_id, leadgen_id} = change.value;
+      console.log(
+        // `Page ID ${141028109311252}, Form ID ${2173107573007712}, Lead gen ID ${leadgen_id}`
+        `Page ID ${page_id}, Form ID ${form_id}, Lead gen ID ${leadgen_id}`
+      );
+
+      // Call graph API to requ est lead info with the lead ID and access token.
+      const leadgenURI = `${GRAPH_API_ENDPOINT}/${leadgen_id}?access_token=${accessToken}`;
+
+      axios(leadgenURI)
+        .then(response => {
+          const {created_time, id, field_data} = response.data
+
+          // Handle lead answer here (insert data into your CRM system)
+          console.log('Lead id', id);
+          console.log('Created time', created_time);
+          field_data.forEach(field => {
+            console.log('Question ', field.name);
+            console.log('Answers ', field.values);
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    });
+  });
+  // Send HTTP 200 OK status to indicate we've received the update.
+  res.sendStatus(200);
+});
+
+// Start web server
+// router.listen(SERVER_PORT, () =>
+//   console.log(`Server is listening at localhost:${SERVER_PORT}`)
+// );
+
+
+
+
+
 //to get total franchisees count
 router.get('/total_franchisees_count/:franchisor_id', function (req, res) {
     // try {
